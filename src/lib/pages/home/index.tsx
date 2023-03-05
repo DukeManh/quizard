bd4abaab59d11489eb545e4e643219f1c4aa6247
DukeManh <manhducdkcb@gmail.com>
2023-03-04 23:49:27 -0500

    add basic quiz selection UI

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,19 +1,100 @@
-import { Flex } from '@chakra-ui/react';
+import {
+  Flex,
+  Text,
+  Select,
+  Button,
+  Container,
+  Center,
+  RadioGroup,
+  Radio,
+  Stack,
+} from '@chakra-ui/react';
 import { NextSeo } from 'next-seo';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+import type { Difficulty, NumQuestion } from '~/types';
+
+type Inputs = {
+  topic: string;
+  difficulty: Difficulty;
+  numberOfQuestions: NumQuestion;
+};
 
 const Home = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  };
+
+  const topics = [
+    'Breaking Bad',
+    'Geography',
+    'French language',
+    'Harry Potter',
+    'Ancient Rome',
+  ];
+
+  const difficulty = ['easy', 'medium', 'hard'];
+
+  const numberOfQuestions = [5, 10, 15, 20];
+
   return (
-    <Flex
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      minHeight="70vh"
-      gap={4}
-      mb={8}
-      w="full"
-    >
+    <Container minH="80vh">
       <NextSeo title="Home" />
-    </Flex>
+      <Text
+        fontSize="4xl"
+        sx={{
+          textAlign: 'center',
+        }}
+        as="h1"
+      >
+        Put your knowledge to the test
+      </Text>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Text>I would like to test my knowledge on:</Text>
+        <Flex direction="column" justifyContent="center" gap={4}>
+          <Select
+            placeholder="Select topic"
+            aria-label="quiz topic"
+            defaultValue={topics[1]}
+            {...register('topic')}
+          >
+            {topics.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </Select>
+          <RadioGroup defaultValue={difficulty[1]}>
+            <Stack direction="row">
+              {difficulty.map((option) => (
+                <Radio key={option} value={option} {...register('difficulty')}>
+                  {option.charAt(0).toUpperCase() + option.slice(1)}
+                </Radio>
+              ))}
+            </Stack>
+          </RadioGroup>
+          <Select
+            placeholder="Number of questions"
+            aria-label="Number of questions"
+            defaultValue={numberOfQuestions[0]}
+            {...register('numberOfQuestions')}
+          >
+            {numberOfQuestions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </Select>
+          <Center>
+            <Button type="submit">Start</Button>
+          </Center>
+        </Flex>
+      </form>
+    </Container>
   );
 };
 
