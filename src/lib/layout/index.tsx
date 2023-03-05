53032f8d4289fdf38a9f41900b29c224f14a6f44
DukeManh <manhducdkcb@gmail.com>
2023-03-04 22:46:15 -0500

    remove commitlint

--- src/lib/layout/index.tsx
+++ src/lib/layout/index.tsx
@@ -10,14 +10,10 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <Box margin="0 auto" maxWidth={800} transition="0.5s ease-out">
-      <Box margin="8">
-        <Header />
-        <Box as="main" marginY={22}>
-          {children}
-        </Box>
-        <Footer />
-      </Box>
+    <Box>
+      <Header />
+      <Box as="main">{children}</Box>
+      <Footer />
     </Box>
   );
 };
